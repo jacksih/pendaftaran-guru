4bcd52fe192c0b10a7e4f1b2d6ebc6663485abae
jacksih <jackysihombing33@gmail.com>
2025-02-11 14:47:03 +0700

    fix gaga login / loop login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,15 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware(function ($request, $next) {
-            if (auth()->user()->role !== 'admin') {
-                abort(403, 'Unauthorized action.');
-            }
-            return $next($request);
-        });
-    }
 }
